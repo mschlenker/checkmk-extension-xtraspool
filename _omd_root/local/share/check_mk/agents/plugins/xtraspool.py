87f1c52560953b33b32d44c2d730ea84667096f2
Mattias Schlenker <mattias.schlenker@checkmk.com>
2026-04-04 12:39:36 +0200

    finish core functionality of the agent plug-in

--- _omd_root/local/share/check_mk/agents/plugins/xtraspool.py
+++ _omd_root/local/share/check_mk/agents/plugins/xtraspool.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# (c) 2025 Mattias Schlenker
+# (c) 2025-2026 Mattias Schlenker
 
 # This script expects a configuration file $MK_CONFDIR/xtraspool.json
 #
@@ -25,7 +25,7 @@ import json
 import re
 import time
 
-# Check for forbidden directories and relative directories
+# Check for forbidden directories or relative pathnames
 
 def check_forbidden(dircfg):
     if not re.compile('^/').match(dircfg['directory']):
@@ -42,7 +42,7 @@ def check_directory_traversal(dircfg):
     ptoks = dircfg['directory'].split('/')
     for d in ptoks:
         if d == "..":
-            raise Exception("FoundDirectoryTraversal")
+            raise Exception("DirectoryTraversalAttempted")
         elif d == ".ssh":
             raise Exception("TryingToAccessSSH")
 
@@ -50,112 +50,109 @@ def check_directory_traversal(dircfg):
 
 def check_filename(dircfg, file):
     if file[0] == '.':
-        return [ 'NoteIgnoreHiddenFile' ]
+        raise Exception("IgnoreHiddenFile")
     p = re.compile('^readme', re.IGNORECASE)
     if p.match(file):
-        return [ 'NoteIgnoreReadMe' ]
-    return []
+        raise Exception("IgnoreReadMe")
 
 # Modification time is not determined by filename, but as a per directory
 # option read from the config
 
-def check_mtime(dircfg, file):
-    errors = []
+def check_age(dircfg, file):
     maxage = 0
     if 'max_age' in dircfg:
         maxage = int(dircfg['max_age'])
     if maxage == 0:
-        return errors
-    fmtime = os.path.getmtime(dircfg['directory'] + '/' + file)
+        return
+    fpath = dircfg['directory'] + '/' + file
+    # At this point, the check whether symlinks are allowed has been passed:
+    fpath = os.path.realpath(fpath)
+    fmtime = os.path.getmtime(fpath)
     now = time.time()
     if (now - fmtime > maxage):
-        errors.append("HintFileTooOld: Last modified {}, max age {}".format(fmtime, maxage))
-    return errors
+        raise Exception("FileTooOld")
 
 # Check the size of a file
 
 def check_size(dircfg, file):
-    errors = []
     maxsize = 0
     if 'ignore_size' in dircfg:
         maxsize = dircfg['ignore_size'] * 1000
     if maxsize == 0:
-        return errors
+        return
     fsize = os.path.getsize(dircfg['directory'] + '/' + file)
     if fsize > maxsize:
-        errors.append("HintFileTooLarge: {} bytes is larger than specified maximum: {}".format(fsize, maxsize)) 
-    return errors
+        raise Exception("FileTooLarge")
 
 # Ignore symlinks if requested
 
 def check_symlink(dircfg, file):
-    errors = []
     ignore_symlinks = False
     if 'follow_symlinks' in dircfg:
         if dircfg['follow_symlinks'] == 'ignore':
             ignore_symlinks = True
     if os.path.islink(dircfg['directory'] + '/' + file) and ignore_symlinks :
-        errors.append("NoteIsSymlink: Requested to ignore symlinks")
-    return errors
-       
-# Check everything above and log errors
-
-def check_everything(dircfg, file):
-    errors = check_symlink(dircfg, file)
-    errors = errors + check_size(dircfg, file)
-    errors = errors + check_filename(dircfg, file)
-    errors = errors + check_mtime(dircfg, file)
-    return errors
+        raise Exception("IgnoreSymlinks")
 
 # Walk through files in a directory and output files
 # according to the config for this directory
 
-def dump_all_files(dircfg, errors):
-    errors[dircfg['directory']] = {}
-    errors[dircfg['directory']]['files'] = []
+def dump_all_files(dircfg, stats):
+    # Return on the first error encountered:
     dir_cont = []
     try:
         check_forbidden(dircfg)
         check_directory_traversal(dircfg)
         dir_cont = os.listdir(dircfg['directory'])
     except Exception as e:
-        errors[dircfg['directory']]['directory'] = str(e)
+        stats["errors"][dircfg['directory']] = str(e)
+        return stats
     buffered = False
     if 'buffering' in dircfg:
         if dircfg['buffering'] == "buffered":
             buffered = True
     for f in dir_cont:
-        e = check_everything(dircfg, f)
-        # print(e)
-        if len(e) < 1:
-            if buffered:
-                e = dump_buffered(dircfg, f)
-            else:
-                e = dump_unbuffered(dircfg, f)
-        if len(e) > 0:
-            errors[dircfg['directory']]['files'].append({ f: e })
-    return errors
+        if buffered:
+            stats = dump_buffered(dircfg, f, stats)
+        else:
+            stats = dump_unbuffered(dircfg, f, stats)
+    return stats
 
 # Buffered output of a single file
 
-def dump_buffered(dircfg, file):
-    errors = []
+def dump_buffered(dircfg, file, stats):
+    fpath = dircfg['directory'] + '/' + file
+    # Check whether to ignore file based on attributes
+    try:
+        check_filename(dircfg, file)
+        check_symlink(dircfg, file)
+    except Exception as e:
+        stats["notes"][fpath] = str(e)
+        return stats
+    # Check whether the file is too big or too old
+    try:
+        check_size(dircfg, file)
+        check_age(dircfg, file)
+    except Exception as e:
+        stats["warnings"][fpath] = str(e)
+        return stats
     lines = []
     inside_piggy = False
+    # Now try to read the whole file as one
     try:
-        f = open(dircfg['directory'] + '/' + file, 'r')
+        f = open(fpath, 'r')
         for line in f:
             lines.append(line.strip('\n'))
         f.close()
     except Exception as e:
-        errors.append(str(e))
-        return errors
+        stats["warnings"][fpath] = str(e)
+        return stats
     ssr = re.compile('^<<<.+>>>$')
     psr = re.compile('^<<<<.+>>>>$')
     per = re.compile('^<<<<>>>>$')
     if not ssr.match(lines[0]):
-        errors.append('ErrorNoAgentSection')
-        return errors
+        stats["warnings"][fpath] = 'ErrorNoAgentSection'
+        return stats
     for l in lines:
         if psr.match(l):
             inside_piggy = True
@@ -164,36 +161,57 @@ def dump_buffered(dircfg, file):
         print(l)
     if inside_piggy == True:
         print('<<<<>>>>')
-        errors.append('WarnFixedPiggybackSection')
-    return errors
+        stats["warnings"][fpath] = 'ErrorNoAgentSection'
+    return stats
 
 # Unbuffered output of a single file
 
-def dump_unbuffered(dircfg, file):
-    errors = []
+def dump_unbuffered(dircfg, file, stats):
+    fpath = dircfg['directory'] + '/' + file
+    # Check whether to ignore file based on attributes
     try:
-        f = open(dircfg['directory'] + '/' + file, 'r')
+        check_filename(dircfg, file)
+        check_symlink(dircfg, file)
     except Exception as e:
-        errors.append(str(e))
-        return errors
+        stats["notes"][fpath] = str(e)
+        return stats
+    # Check whether the file is too big or too old
+    try:
+        check_size(dircfg, file)
+        check_age(dircfg, file)
+    except Exception as e:
+        stats["warnings"][fpath] = str(e)
+        return stats
+    # Now try to read the file line by line, immediately emitting
+    try:
+        f = open(fpath, 'r')
+    except Exception as e:
+        stats["errors"][fpath] = str(e)
+        return stats
     try:
         for line in f:
             print(line, end='')
     except Exception as e:
         f.close
-        errors.append(str(e))
-        return errors
+        stats["errors"][fpath] = str(e)
+        return stats
     f.close
-    return errors
+    return stats
 
-errors = {}
+stats = {
+    "notes" : {},
+    "warnings" : {},
+    "errors" : {}
+}
 cfgfile = os.environ['MK_CONFDIR'] + '/xtraspool.json'
-with open(cfgfile) as f:
-    cfg = json.load(f)
-
-for dir in cfg['dirs']:
-    errors = dump_all_files(dir, errors)
+if not os.path.exists(cfgfile):
+    stats["errors"][cfgfile] = "ConfigFileMissing"
+else:
+    with open(cfgfile) as f:
+        cfg = json.load(f)
+    for dir in cfg['dirs']:
+        stats = dump_all_files(dir, stats)
 
 # Output statistics, to be later used by an agent plugin
 print('<<<xtraspool_stats>>>')
-print(json.dumps(errors))
+print(json.dumps(stats))
